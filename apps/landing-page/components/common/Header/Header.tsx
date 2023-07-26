41371c01d6591a38ce1b79e47c0edf0ac42f067a
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-07-26 11:29:57 -0400

    Update Header.tsx

--- apps/landing-page/components/common/Header/Header.tsx
+++ apps/landing-page/components/common/Header/Header.tsx
@@ -43,7 +43,7 @@ export const Header = () => {
           <HStack as={Link} href="/" rel="home" ml="2">
             <Logo boxSize="35px" />
             <Heading as="p" fontSize="lg">
-              Typebot
+              Chatworth
             </Heading>
           </HStack>
         </Flex>
@@ -87,7 +87,7 @@ export const Header = () => {
           </Button>
           <Button
             as={Link}
-            href="https://app.typebot.io/signin"
+            href="https://app.chatworth.io/signin"
             colorScheme="blue"
             variant="outline"
             fontWeight={700}
@@ -96,7 +96,7 @@ export const Header = () => {
           </Button>
           <Button
             as={Link}
-            href="https://app.typebot.io/register"
+            href="https://app.chatworth.io/register"
             colorScheme="orange"
             fontWeight={700}
           >
