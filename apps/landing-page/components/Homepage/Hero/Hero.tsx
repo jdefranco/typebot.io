d9e4873bf89661c17354cb2d0e913d53ba59d112
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-07-27 13:22:45 -0400

    Update Hero.tsx
    delete companies

--- apps/landing-page/components/Homepage/Hero/Hero.tsx
+++ apps/landing-page/components/Homepage/Hero/Hero.tsx
@@ -12,7 +12,6 @@ import {
 import * as React from 'react'
 import { Header } from '../../common/Header/Header'
 import { BackgroundPolygons } from './BackgroundPolygons'
-import * as Logos from './Brands'
 import Link from 'next/link'
 
 export const Hero = () => {
@@ -50,7 +49,7 @@ export const Hero = () => {
             >
               <Button
                 as={Link}
-                href="https://app.typebot.io/register"
+                href="https://app.chatworth.io/register"
                 colorScheme="orange"
                 size="lg"
                 height="4rem"
@@ -101,30 +100,6 @@ export const Hero = () => {
           </Box>
         </VStack>
       </Stack>
-      <Flex justify="center" bgGradient="linear(to-b, gray.900, gray.800)">
-        <VStack spacing="12" pb="32" maxW="7xl" px={4}>
-          <Heading fontSize="25px" fontWeight="semibold" data-aos="fade">
-            Trusted by Teams and Creators Globally
-          </Heading>
-          <SimpleGrid
-            columns={{ base: 2, md: 4 }}
-            color="gray.400"
-            alignItems="center"
-            spacing={12}
-            fontSize="4xl"
-            data-aos="fade"
-          >
-            <Logos.IbanFirst />
-            <Logos.Lemlist />
-            <Logos.MakerLead />
-            <Logos.Webisharp />
-            <Logos.SocialHackrs />
-            <Logos.PinpointInteractive />
-            <Logos.Obole />
-            <Logos.Awwwsome />
-          </SimpleGrid>
-        </VStack>
-      </Flex>
     </Box>
   )
 }
