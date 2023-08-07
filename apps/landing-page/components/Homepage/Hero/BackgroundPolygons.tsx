bd033976ca5576480c8423ef9a04b0e25fe67554
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-08-07 01:36:19 -0400

    Update BackgroundPolygons.tsx

--- apps/landing-page/components/Homepage/Hero/BackgroundPolygons.tsx
+++ apps/landing-page/components/Homepage/Hero/BackgroundPolygons.tsx
@@ -163,22 +163,9 @@ const DemiCircle = () => (
   </svg>
 )
 const Triangle = () => (
- <svg
-   xmlns:dc="http://purl.org/dc/elements/1.1/"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:xlink="http://www.w3.org/1999/xlink"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   width="210mm"
-   height="297mm"
-   viewBox="0 0 210 297"
-   version="1.1"
-   id="svg3631"
-   inkscape:version="0.92.4 (5da689c313, 2019-01-14)"
-   sodipodi:docname="moon stars.svg">
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="210mm"
+   height="297mm" viewBox="0 0 210 297">
   <defs
      id="defs3625">
     <clipPath
