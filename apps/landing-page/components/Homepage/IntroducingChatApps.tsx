30435dc30533a25e157634e842183fd3979b613e
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-08-06 22:10:01 -0400

    Update IntroducingChatApps.tsx

--- apps/landing-page/components/Homepage/IntroducingChatApps.tsx
+++ apps/landing-page/components/Homepage/IntroducingChatApps.tsx
@@ -9,11 +9,11 @@ import {
   Input,
   Checkbox,
   Textarea,
-} from '@chakra-ui/react'
-import React from 'react'
-import { DontIcon } from 'assets/icons/DontIcon'
-import { DoIcon } from 'assets/icons/DoIcon'
-import { HandDrawnArrow } from 'assets/illustrations/HandDrawnArrow'
+} from '@chakra-ui/react';
+import React from 'react';
+import { DontIcon } from 'assets/icons/DontIcon';
+import { DoIcon } from 'assets/icons/DoIcon';
+import { HandDrawnArrow } from 'assets/illustrations/HandDrawnArrow';
 
 export const IntroducingChatApps = () => {
   return (
@@ -68,7 +68,7 @@ export const IntroducingChatApps = () => {
               src="https://bot.chatworth.io/chatworth-ai-a8up210" 
               width="100%" 
               height="562px" 
-              frameborder="0"
+              frameBorder="0"
               style={{
                 borderRadius: '0.375rem',
                 borderWidth: '1px',
@@ -90,7 +90,7 @@ export const IntroducingChatApps = () => {
         </Stack>
       </Stack>
     </Flex>
-  )
+  );
 }
 
 const FakeLeadGenForm = () => (
@@ -122,4 +122,4 @@ const FakeLeadGenForm = () => (
       <Button>Submit</Button>
     </Flex>
   </Stack>
-)
+);
