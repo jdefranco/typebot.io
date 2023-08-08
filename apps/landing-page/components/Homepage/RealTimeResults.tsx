5bea10cb63a1ecc1d07743977c725cd44b9fb522
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-08-08 15:47:03 -0400

    Update RealTimeResults.tsx

--- apps/landing-page/components/Homepage/RealTimeResults.tsx
+++ apps/landing-page/components/Homepage/RealTimeResults.tsx
@@ -8,16 +8,10 @@ export const RealTimeResults = () => {
     const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
     useEffect(() => {
-        console.log("Setting up event listener for postMessage.");
-
+        // Listener for the postMessage event
         window.addEventListener('message', function(event) {
-            // Check if the message is coming from our chatbot's domain
-            if (event.origin !== "https://bot.chatworth.io") return;
-
-            console.log("Received message from iframe:", event.data);
-
             // Check if the received message is the one we're expecting
-            if (event.data && event.data.from === 'chatworth') {
+            if (event.data && event.data.from === 'chatworth' && event.data.action === 'refresh-airtable') {
                 console.log("Refreshing Airtable iframe.");
                 // Refresh the Airtable iframe
                 if (iframeRef.current) {
@@ -28,10 +22,6 @@ export const RealTimeResults = () => {
             }
         });
     }, []);
-
-
-
-
 
     return (
         <Flex as="section" justify="center">
