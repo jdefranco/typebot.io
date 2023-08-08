0ef844330fd1956fd5118265c14892ee520cf1e0
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-08-08 17:05:50 -0400

    Update RealTimeResults.tsx

--- apps/landing-page/components/Homepage/RealTimeResults.tsx
+++ apps/landing-page/components/Homepage/RealTimeResults.tsx
@@ -1,5 +1,5 @@
 import { Flex, Stack, Heading, Text, Button, VStack } from '@chakra-ui/react';
-import { Standard } from '@typebot.io/react';
+import { Standard } from '@typebot.io/react'; // Corrected import
 import { ArrowRight } from 'assets/icons/ArrowRight';
 import { HandDrawnArrow } from 'assets/illustrations/HandDrawnArrow';
 import Link from 'next/link';
@@ -13,7 +13,6 @@ const messageBlockId = 'qqbm08tbeorqisluz3g99bq8';
 export const RealTimeResults = () => {
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const [typebot, setTypebot] = useState<PublicTypebot>();
-  const [isLoading, setIsLoading] = useState(false); // State for loading placeholder
 
   const fetchTemplate = async () => {
     const { data, error } = await sendRequest(`/typebots/realtime-airtable.json`);
@@ -28,8 +27,6 @@ export const RealTimeResults = () => {
 
   const refreshIframeContent = () => {
     if (!iframeRef.current) return;
-    setIsLoading(true);
-    iframeRef.current.onload = () => setIsLoading(false);
     iframeRef.current.src += '';
   };
 
@@ -38,81 +35,94 @@ export const RealTimeResults = () => {
       setTimeout(refreshIframeContent, 1000);
   };
 
-  return (
-    <Flex as="section" justify="center">
-      <Stack
-        style={{ maxWidth: '1200px' }}
-        pt={'52'}
-        w="full"
-        px="4"
-        spacing={16}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <VStack spacing={6}>
-          {/* ... rest of your code for the heading and button ... */}
-        </VStack>
+    return (
+        <Flex as="section" justify="center">
+            <Stack
+                style={{ maxWidth: '1200px' }}
+                pt={'52'}
+                w="full"
+                px="4"
+                spacing={16}
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <VStack spacing={6}>
+                    <Heading
+                        fontSize={{ base: '4xl', xl: '6xl' }}
+                        textAlign="center"
+                        data-aos="fade"
+                    >
+                        Real-Time Data Collection, Enlightened by AI
+                    </Heading>
+                    <Text
+                        textAlign="center"
+                        color="gray.400"
+                        maxW="1000px"
+                        fontSize={{ base: 'lg', xl: 'xl' }}
+                        data-aos="fade"
+                    >
+                        The power of a chat application lies in its ability to gather data in real-time. With Chatworth, you not only collect data but also gain insights, ensuring no valuable information is lost in the ether.
+                    </Text>
+                    <Flex>
+                        <Button
+                            as={Link}
+                            rightIcon={<ArrowRight />}
+                            href={`https://app.chatworth.io/register`}
+                            variant="ghost"
+                            colorScheme="blue"
+                            data-aos="fade"
+                        >
+                            Try it now
+                        </Button>
+                    </Flex>
+                </VStack>
 
-        <Stack w="full" direction={['column', 'row']} spacing="4" data-aos="fade">
-          {typebot && (
-            <Standard
-              typebot={typebot.typebotId} // Ensure the typebot ID is passed correctly
-              apiHost="https://bot.chatworth.io"
-              onAnswer={handleAnswer}
-              style={{
-                borderRadius: '0.375rem',
-                borderWidth: '1px',
-                width: '100%', // Ensure full width
-                height: '533px',
-              }}
-            />
-          )}
-          <div style={{ position: 'relative', width: '100%' }}> {/* Ensure full width */}
-            <iframe
-              ref={iframeRef}
-              src="https://airtable.com/embed/app19rMMlM3AunNmT/shraPEcx5DKCdudmF?backgroundColor=cyan"
-              width="100%"
-              height="533"
-              style={{
-                borderRadius: '0.5rem',
-                border: 'none',
-                backgroundColor: 'white',
-              }}
-            />
-            {isLoading && (
-              <div style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'rgba(255, 255, 255, 0.8)' // semi-transparent white
-              }}>
-                Loading...
-              </div>
-            )}
-          </div>
-          <Flex
-            top="-60px"
-            right="-30px"
-            pos="absolute"
-            display={{ base: 'none', xl: 'flex' }}
-          >
-            <Text fontFamily="'Indie Flower'" fontSize="2xl">
-              It&apos;s a real Airtable view!
-            </Text>
-            <HandDrawnArrow
-              transform="rotate(30deg)"
-              boxSize="100px"
-              top="15px"
-              right="-60px"
-              pos="absolute"
-            />
-          </Flex>
-        </Stack>
-      </Stack>
-    </Flex>
-  );
+                <Stack
+                    w="full"
+                    direction={['column', 'row']}
+                    spacing="4"
+                    data-aos="fade"
+                >
+                    <Standard
+                        typebot="my-typebot-orp4iiy"
+                        apiHost="https://bot.chatworth.io" // Update this to the correct Chatworth API endpoint
+                        onAnswer={handleAnswer}
+                        style={{
+                            borderRadius: '0.375rem',
+                            borderWidth: '1px',
+                            height: '533px',
+                        }}
+                    />
+                    <iframe
+                        ref={iframeRef}
+                        src="https://airtable.com/embed/app19rMMlM3AunNmT/shraPEcx5DKCdudmF?backgroundColor=cyan"
+                        width="100%"
+                        height="533"
+                        style={{
+                            borderRadius: '0.5rem',
+                            border: 'none',
+                            backgroundColor: 'white',
+                        }}
+                    />
+                    <Flex
+                        top="-60px"
+                        right="-30px"
+                        pos="absolute"
+                        display={{ base: 'none', xl: 'flex' }}
+                    >
+                        <Text fontFamily="'Indie Flower'" fontSize="2xl">
+                            It&apos;s a real Airtable view!
+                        </Text>
+                        <HandDrawnArrow
+                            transform="rotate(30deg)"
+                            boxSize="100px"
+                            top="15px"
+                            right="-60px"
+                            pos="absolute"
+                        />
+                    </Flex>
+                </Stack>
+            </Stack>
+        </Flex>
+    );
+};
