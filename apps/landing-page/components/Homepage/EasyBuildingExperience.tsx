d308721e836a81bed079312fbf2db9ad37e5e859
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-09-12 22:15:55 -0400

    new changes to this

--- apps/landing-page/components/Homepage/EasyBuildingExperience.tsx
+++ apps/landing-page/components/Homepage/EasyBuildingExperience.tsx
@@ -1,9 +1,9 @@
-import { Flex, Stack, Heading, Box, Text, Button } from '@chakra-ui/react'
-import React from 'react'
-import Image from 'next/image'
-import { ArrowRight } from 'assets/icons/ArrowRight'
-import { Flare } from 'assets/illustrations/Flare'
-import Link from 'next/link'
+import { Flex, Stack, Heading, Box, Text, Button } from '@chakra-ui/react';
+import React from 'react';
+import Image from 'next/image';
+import { ArrowRight } from 'assets/icons/ArrowRight';
+import { Flare } from 'assets/illustrations/Flare';
+import Link from 'next/link';
 
 export const EasyBuildingExperience = () => {
   return (
@@ -35,9 +35,9 @@ export const EasyBuildingExperience = () => {
             fontSize={{ base: 'lg', xl: 'xl' }}
             data-aos="fade"
           >
-With Chatworth, craft dynamic chatbots using a rich set of modules. From text bubbles, audio, and video to diverse inputs like email, date, and payment. All designed for an engaging user interaction.
-          </Text> 
-<Flex>
+            With Chatworth, craft dynamic chatbots using a rich set of modules. From text bubbles, audio, and video to diverse inputs like email, date, and payment. All designed for an engaging user interaction.
+          </Text>
+          <Flex>
             <Button
               as={Link}
               rightIcon={<ArrowRight />}
@@ -50,27 +50,24 @@ With Chatworth, craft dynamic chatbots using a rich set of modules. From text bu
           </Flex>
         </Stack>
         <Box 
-    data-aos="fade"
-    position="relative"
-    p="4px"
-    borderWidth="4px"  // Border width
-    style={{ borderRadius: '8px' }} 
-    borderColor="royalPurple.300"  // Border color
-    bg="#EFEFEF"  // Background color set to white using hex code
-    rounded="xl"  // Border radius for the box\ 
->
-    <Image
-        src="/easy-build.png"
-        alt="incomplete results illustration"
-        placeholder="blur"
-        rounded="xl"  // Ensure the image border radius matches the box
-        display="block"
-        w="100%"  // Ensure the image takes up the full width
-        h="100%"  // Ensure the image takes up the full height
-    />
-</Box>
-
+          data-aos="fade"
+          position="relative"
+          p="4px"
+          borderWidth="4px"
+          borderColor="royalPurple.300"
+          bg="#EFEFEF"
+          rounded="xl"
+        >
+          <Image
+            src="/easy-build.png"
+            alt="incomplete results illustration"
+            placeholder="blur"
+            display="block"
+            width={1000}  // Adjust this value to the actual width of your image
+            height={1000}  // Adjust this value to the actual height of your image
+          />
+        </Box>
       </Stack>
     </Flex>
-  )
+  );
 }
